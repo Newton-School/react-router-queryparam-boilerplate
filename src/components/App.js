92c54f20a793476749897aabd5bd4db3f04974fa
Kush Vasaniya <vasaniyakush@gmail.com>
2023-03-02 18:51:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "../styles/App.css";
-// import React from "react";
-// import "./App.css"
 import { BrowserRouter as Router,Link, Route, Switch, useLocation } from "react-router-dom";
 import Home from "./Home";
 import Shapes from "./Shapes";
@@ -12,43 +10,40 @@ import Image from "./Image";
 
 export default function App() {
   return (
-    <Router>
+    
     <div id="home">
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li>
-            <Link to="/">Home</Link>
+            {/* Home*/}
           </li>
           <li>
-            <Link to="/Shapes" >Shapes</Link>
+            {/* Shapes*/}
           </li>
           <li>
-            <Link to="/Image">Image</Link>
+           {/* Image*/}
           </li>
         </ul>
       </nav>
-      {/* Route components are rendered if the path prop matches the current URL */}
-      <Switch>
+      
+      
+          
+        
 
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route  path="/Shapes">
+        
           
             
-              <Shapes />
+              
             
          
-        </Route>
+      
         
       
-        <Route path="/Image">
-          <Image />
-        </Route>
-      </Switch>
+        
+        
+      
     </div>
-      </Router>
+      
   );
 }
 // export default App;
